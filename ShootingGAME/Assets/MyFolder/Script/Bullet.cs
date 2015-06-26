901859082d4dce7bcf5a4fe83457e7275327b65b
NissyY <yuyunshishi@gmail.com>
2015-06-26 14:13:36 +0900

    make Stage & make Enemy & make Shoot

---
+++ ShootingGAME/Assets/MyFolder/Script/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	PlayerControl playercontrol;
+
+	float bulletSpeed = 0.5f;
+	
+	void Start () {
+		Destroy (this.gameObject, 4);//own destroy after 5seconds
+	}
+
+	void Update () {
+		transform.Translate (0, 0, bulletSpeed);//go z direction: speed = bulletSpeed 
+	}
+}
---
+++ ShootingGAME/Assets/MyFolder/Script/EnemyControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyControl : MonoBehaviour {
+
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		transform.Rotate (0, 0.5f, 0);
+	}
+}
